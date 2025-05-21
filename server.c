a340dec080b0db8cd28cdd169b6dfe306f8338d1
andres <fmendez1212@gmail.com>
2025-05-21 19:22:41 +0100

    change port

--- server.c
+++ server.c
@@ -10,7 +10,7 @@
 int main() {
     struct sockaddr_in addr = {};
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(8080);
+    addr.sin_port = htons(8000);
     addr.sin_addr.s_addr = INADDR_ANY;
 
     int s = socket(AF_INET, SOCK_STREAM,0);
